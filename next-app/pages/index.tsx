8d79e4af14ca93b425955550f04e292c396ecee6
Brian Keeter <brian@heydev.com>
2020-04-09 10:57:59 -0500

    initial commit with nextjs app

---
+++ next-app/pages/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Link from 'next/link';
+
+const PostLink = (props:any) =>
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+
+const Home = () =>
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>;
+
+export default Home;
